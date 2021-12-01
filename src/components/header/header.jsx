77a4f475d0bbe4603cbb2c781f089c8eae7d9cfc
James Larson <imjameslarson@gmail.com>
2021-11-30 17:08:17 -0700

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import * as S from './styles';
+import ACRLogo from '../../images/ACRLogo.png';
+import Hamburger from '../../icons/hamburger.png';
+import MagnifyingGlass from '../../icons/magnifying-glass.png';
 
+// src/images/ACRLogo.png
 const Header = () => {
   return (
     <S.HeaderWrap>
-      <S.P>header</S.P>
+      <S.HeaderTop>
+        <S.HeaderSectionInner>
+          <S.TopLinksWrapper>
+            <S.TopLink>Membership</S.TopLink>
+            <S.TopLink>Patient Resources</S.TopLink>
+            <S.TopLink>Whats New</S.TopLink>
+            <S.TopLink>Journals</S.TopLink>
+            <S.TopLink>Global Community</S.TopLink>
+            <S.TopLink>Donate</S.TopLink>
+          </S.TopLinksWrapper>
+          <S.TopLinksWrapper>
+            <S.TopLink right>Login</S.TopLink>
+          </S.TopLinksWrapper>
+        </S.HeaderSectionInner>
+      </S.HeaderTop>
+      <S.HeaderBottom>
+        <S.HeaderSectionInner>
+          <S.HeaderButton>
+            <S.ButtonIcon src={Hamburger} />
+            <S.ButtonText>MENU</S.ButtonText>
+          </S.HeaderButton>
+          <S.Logo src={ACRLogo} />
+          <S.HeaderButton>
+            <S.ButtonIcon src={MagnifyingGlass} />
+            <S.ButtonText>SEARCH</S.ButtonText>
+          </S.HeaderButton>
+        </S.HeaderSectionInner>
+      </S.HeaderBottom>
     </S.HeaderWrap>
   );
 };
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,9 +1,70 @@
 import styled from 'styled-components'
 
+export const ButtonIcon = styled.img`
+  margin-right: 1rem;
+`;
+
+export const ButtonText = styled.p`
+  color: #535A8D;
+  font-size: 1.4rem;
+  font-weight: 700;
+  margin: 0;
+`;
+
+export const HeaderBottom = styled.div`
+  border-bottom: .1rem solid #D0D2D7;  
+  display: flex;
+  height: 8.8rem;
+  justify-content: center;
+`;
+
+export const HeaderButton = styled.div`
+  align-items: center;
+  border: .1rem solid #BCC1DC;
+  border-radius: .8rem;
+  display: flex;
+  flex-direction: row;
+  height: 4rem;
+  justify-content: space-between;
+  padding: 0 1.2rem;
+`;
+
+export const HeaderSectionInner = styled.div`
+  align-items:center;  
+  display: flex;
+  justify-content: space-between;
+  width: 144rem;
+`;
+
+export const HeaderTop = styled.div`
+  border-bottom: .1rem solid #E9EAEC;
+  display: flex;
+  height: 4rem;
+  justify-content: center;
+`;
+
 export const HeaderWrap = styled.div`
   width: 100%;
 `;
 
-export const P = styled.p`
-  font-size: 2rem;
+export const Logo = styled.img`
+  height: 6.3rem;
+`;
+
+export const TopLink = styled.li`
+  color: #535A8D;  
+  font-size: 1.3rem;
+  margin-right: ${props => props.right ? '0' : '1.6rem'};
+
+  &:hover{ 
+    cursor: pointer; //I know this isnt a link! Leave me alone!
+  }
+`;
+
+export const TopLinksWrapper = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `;
